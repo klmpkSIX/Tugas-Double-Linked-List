02c0a464b6ef9277e97f214978ac41a5523e6a8f
THUR <114821090+klmpkSIX@users.noreply.github.com>
2023-11-20 00:09:43 +0700

    Update DoubleLinkedList.java

--- DoubleLinkedList.java
+++ DoubleLinkedList.java
@@ -1,16 +1,3 @@
-
-public class Node {
-    public int data;
-    public Node next;
-    public Node prev;
-
-    public Node(int data) {
-        this.data = data;
-        this.next = null;
-        this.prev = null;
-    }
-}
-
 public class DoubleLinkedList {
     private Node head;
     private Node tail;
@@ -64,25 +51,5 @@ public class DoubleLinkedList {
             current = current.next;
         }
         System.out.println();
-    }
-}
-
-public class Main {
-    public static void main(String[] args) {
-        DoubleLinkedList dll = new DoubleLinkedList();
-
-        dll.insertAtEnd(10);
-        dll.insertAtBeginning(5);
-        dll.insertAtEnd(20);
-        dll.insertAtBeginning(2);
-
-        System.out.println("Linked List from front to back:");
-        dll.displayForward();
-
-        Node currentNode = dll.readNext(dll.readNext(dll.readNext(dll.readNext(null))));
-        System.out.println("Data in the next node: " + (currentNode != null ? currentNode.data : "No next node"));
-
-        Node previousNode = dll.readPrevious(dll.readPrevious(dll.readPrevious(dll.readPrevious(null))));
-        System.out.println("Data in the previous node: " + (previousNode != null ? previousNode.data : "No previous node"));
     }
 }
